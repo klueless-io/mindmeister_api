70019d4c1c2d8b54da4016176721edca7ce0db21
David Cruwys <david@ideasmen.com.au>
2021-06-13 08:13:27 +1000

    spike for reading mindmaps and uncompressing zip

--- lib/mindmeister_api.rb
+++ lib/mindmeister_api.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
-require_relative "mindmeister_api/version"
+require 'json'
+require 'net/http'
+require 'zip'
+
+require_relative 'mindmeister_api/version'
 
 module MindmeisterApi
   class Error < StandardError; end
